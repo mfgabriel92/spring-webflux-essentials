a999c96360e1c146e2c8b5008894068e5de29464
Gabriel Monteiro Fernandes <mfgabriel92@gmail.com>
2020-09-29 10:43:29 -0300

    SwitchIfEmpty and ResponseStatusException

--- src/main/java/br/gabriel/springwebfluxessentials/service/AnimeService.java
+++ src/main/java/br/gabriel/springwebfluxessentials/service/AnimeService.java
@@ -3,7 +3,9 @@ package br.gabriel.springwebfluxessentials.service;
 import br.gabriel.springwebfluxessentials.domain.Anime;
 import br.gabriel.springwebfluxessentials.repository.AnimeRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -12,11 +14,17 @@ import reactor.core.publisher.Mono;
 public class AnimeService {
     private final AnimeRepository repository;
     
-    public Mono<Anime> findById(Integer id) {
-        return repository.findById(id);
-    }
-    
     public Flux<Anime> findAll() {
         return repository.findAll();
+    }
+    
+    public Mono<Anime> findById(Integer id) {
+        return repository
+            .findById(id)
+            .switchIfEmpty(notFound());
+    }
+    
+    private <T> Mono<T> notFound() {
+        return Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND, "Anime not found"));
     }
 }
